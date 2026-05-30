8db10743292d36380b66c354b1008be992bd81e2
서지호 <tenkkang111@gmail.com>
2026-05-31 00:36:17 +0900

    defalt typedef

---
+++ include/common.h
@@ -0,0 +1,31 @@
+#include <stdint.h>
+#include <stdbool.h>
+
+// 블럭 타입 정의
+typedef enum {
+    EMPTY = 0, I, J, L, O, S, T, Z, GARBAGE
+} BlockType;
+
+// 현재 블럭 정보
+typedef struct {
+    int8_t x, y;
+    BlockType type;
+    uint8_t rotation;
+} CurrentBlock;
+
+// 7-Bag 시스템 관리 구조체
+typedef struct {
+    BlockType bag[7];
+    uint8_t currentIndex;
+    BlockType nextBag[7];
+} BagSystem;
+
+// 게임 상태 구조체
+typedef struct {
+    uint8_t board[20][10];   // 20*10 게임 보드
+    CurrentBlock activeBlock;// 활성 블럭 정보
+    BagSystem bagState;      // 7-Bag 정보
+    uint32_t score;          // 점수
+    uint8_t pendingGarbage;  // 대기 가비지 라인 수(Multi 용도)
+    bool isGameOver;         // 게임 종료 여부
+} GameState;
